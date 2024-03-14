05fce160f462ef87f20afc92665bd542decbdf18
Mahmoud Salah <ms685116@gmail.com>
2024-03-14 16:50:22 +0200

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,18 +2,15 @@
 """lockboxes problem"""
 
 
-opened = set()
-
-
 def canUnlockAll(boxes):
     """check if all boxes can be opened"""
-    boxes_len = set(range(1, len(boxes)))
+    opened = set(boxes[0])
     for i, box in enumerate(boxes):
-        if len(box) == 0 and not i == len(boxes) - 1:
-            return False
+        if len(box) == 0:
+            if i != len(boxes) - 1:
+                return False
+            continue
         for key in box:
             if key not in opened:
                 opened.add(key)
-    if boxes_len.issubset(opened):
-        return True
-    return False
+    return set(range(1, len(boxes))).issubset(opened)
